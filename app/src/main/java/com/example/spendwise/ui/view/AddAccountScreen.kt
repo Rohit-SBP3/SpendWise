9fbf1ef628843de6c8db2d1f04e21e3c40289442
Rohit Singh <rohitrajput95547@gmail.com>
2025-01-02 21:00:44 +0530

    AddAccountScreen and AddCategoryScreen

--- app/src/main/java/com/example/spendwise/ui/view/AddAccountScreen.kt
+++ app/src/main/java/com/example/spendwise/ui/view/AddAccountScreen.kt
@@ -11,12 +11,19 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Create
+import androidx.compose.material3.Checkbox
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -33,6 +40,8 @@ fun AddAccountScreen(modifier: Modifier = Modifier){
         UpperNavigation(modifier)
         Spacer(modifier = modifier.height(15.dp))
         AddAccountDetails(modifier)
+        Balance(modifier)
+        CurrencyAndExclude(modifier)
     }
 }
 
@@ -68,5 +77,72 @@ fun AddAccountDetails(modifier: Modifier = Modifier){
             fontSize = 14.sp,
             modifier = modifier.padding(top = 12.dp)
         )
+    }
+}
+
+@Composable
+fun Balance(modifier: Modifier = Modifier){
+
+    Column(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(top = 20.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ){
+        Text(
+            text = "$0.00",
+            fontSize = 40.sp,
+            fontWeight = FontWeight.Bold
+        )
+        Spacer(modifier = modifier.height(8.dp))
+        Text(
+            text = "Update balance",
+            fontSize = 16.sp,
+        )
+    }
+}
+
+@Composable
+fun CurrencyAndExclude(modifier: Modifier = Modifier){
+
+    var isChecked by remember { mutableStateOf(false) }
+
+    Column(
+        modifier
+            .padding(start = 10.dp, top = 50.dp)
+            .fillMaxWidth(),
+        verticalArrangement = Arrangement.SpaceBetween,
+    ) {
+        Row(
+            modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = "Exclude from balance",
+                fontSize = 20.sp,
+            )
+            Checkbox(
+                checked = isChecked,
+                onCheckedChange = {isChecked = it}
+            )
+        }
+        Row (
+            modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ){
+            Text(
+                text = "Currency",
+                fontSize = 20.sp,
+            )
+            Spacer(modifier = modifier.height(80.dp))
+            Text(
+                text = "USD",
+                fontSize = 18.sp,
+                modifier = modifier.padding(end = 10.dp)
+            )
+        }
     }
 }
---
+++ app/src/main/java/com/example/spendwise/ui/view/AddCategoryScreen.kt
@@ -0,0 +1,70 @@
+package com.example.spendwise.ui.view
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.sharp.AddCircle
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Preview(showBackground = true)
+@Composable
+fun AddCategoryScreen(modifier: Modifier = Modifier){
+
+    var categoryName by remember { mutableStateOf("") }
+
+    Column(
+        modifier
+            .fillMaxSize()
+            .padding(10.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        UpperNavigation(modifier)
+        Spacer(modifier = modifier.height(20.dp))
+        Icon(
+            imageVector = Icons.Sharp.AddCircle,
+            contentDescription = "Add Image",
+            modifier.size(120.dp)
+        )
+        Text(
+            text = "Change icon",
+            fontSize = 19.sp,
+            textAlign = TextAlign.Center
+        )
+        Spacer(modifier = modifier.height(20.dp))
+        TextField(
+            value = categoryName,
+            onValueChange = {categoryName = it },
+            label = { Text("Category name") },
+            placeholder = { Text(categoryName) },
+            singleLine = true,
+            textStyle = TextStyle(fontWeight = FontWeight.Bold, fontSize = 20.sp),
+            modifier = modifier.fillMaxWidth(),
+            colors = TextFieldDefaults.colors(
+                unfocusedContainerColor = Color(0xFFFFFFFF)
+            )
+        )
+    }
+}
