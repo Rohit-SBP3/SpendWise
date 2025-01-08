7f0de79f36aa395d33d3d543931bff75c7ecc97c
Rohit Singh <rohitrajput95547@gmail.com>
2025-01-08 22:25:12 +0530

    ImportScreen

--- app/src/main/java/com/example/spendwise/ui/view/ImportScreen.kt
+++ app/src/main/java/com/example/spendwise/ui/view/ImportScreen.kt
@@ -1,2 +1,23 @@
 package com.example.spendwise.ui.view
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+@Preview(showBackground = true)
+@Composable
+fun ImportScreen(){
+    Column(
+        modifier = Modifier.fillMaxSize()
+    ){
+        UpperBarWithIconAndText(
+            leadIcon = Icons.Default.ArrowBack,
+            trailIcon = null,
+            text = "Import"
+        )
+    }
+}
