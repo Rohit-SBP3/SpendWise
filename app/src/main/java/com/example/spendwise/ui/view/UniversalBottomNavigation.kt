1972182bf52d9844d957c44630c773e6cea840a9
Rohit Singh <rohitrajput95547@gmail.com>
2025-02-09 19:51:33 +0530

    Screen and Icons

--- app/src/main/java/com/example/spendwise/ui/view/UniversalBottomNavigation.kt
+++ app/src/main/java/com/example/spendwise/ui/view/UniversalBottomNavigation.kt
@@ -1,24 +1,28 @@
 package com.example.spendwise.ui.view
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Home
-import androidx.compose.material.icons.filled.Person
-import androidx.compose.material.icons.filled.Search
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
-import androidx.navigation.compose.*
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
+import com.example.spendwise.R
 
 @Composable
 fun BottomNavBarApp() {
@@ -43,7 +47,9 @@ fun BottomNavBarApp() {
 fun BottomNavBar(navController: NavController) {
     val items = listOf(
         Screen.Home,
-        Screen.Search,
+        Screen.Stats,
+        Screen.Add,
+        Screen.Wallet,
         Screen.Profile
     )
 
@@ -51,26 +57,35 @@ fun BottomNavBar(navController: NavController) {
         val currentRoute = navController.currentBackStackEntryAsState().value?.destination?.route
 
         items.forEach { screen ->
-            NavigationBarItem(
-                icon = { Icon(screen.icon, contentDescription = screen.label) },
-                label = { Text(screen.label) },
-                selected = currentRoute == screen.route,
-                onClick = { navController.navigate(screen.route) }
-            )
+
+
+                NavigationBarItem(
+                    icon = {
+                        Icon(
+                            painter = screen.icon(),
+                            contentDescription = "Icon",
+                            modifier = Modifier.size(30.dp)
+                        ) },
+                    label = { Text(screen.label) },
+                    selected = currentRoute == screen.route,
+                    onClick = { navController.navigate(screen.route) }
+                )
         }
     }
 }
 
 // Sealed class to define navigation items
-sealed class Screen(val route: String, val label: String, val icon: androidx.compose.ui.graphics.vector.ImageVector) {
-    object Home : Screen("home", "Home", Icons.Filled.Home)
-    object Search : Screen("search", "Search", Icons.Filled.Search)
-    object Profile : Screen("profile", "Profile", Icons.Filled.Person)
+sealed class Screen(val route: String, val label: String, val icon: @Composable () -> Painter) {
+    object Home : Screen("home", "Home",  { painterResource(R.drawable.home) })
+    object Stats : Screen("stat", "Stats",  { painterResource(R.drawable.barchart) })
+    object Add : Screen("add", "Add",  { painterResource(R.drawable.add) })
+    object Wallet : Screen("wallet", "Wallet", { painterResource(R.drawable.wallet) })
+    object Profile : Screen("profile", "Profile",  { painterResource(R.drawable.user) })
 }
 
 @Composable
 fun ScreenContent(text: String) {
-    Box(modifier = Modifier.fillMaxSize(), contentAlignment = androidx.compose.ui.Alignment.Center) {
+    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
         Text(text)
     }
 }
